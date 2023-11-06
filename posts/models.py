b313a97ab7abfd50a021b9f02ca1b76e15c4e879
Gouda01 <goda61@gmail.com>
2023-11-06 22:51:26 +0200

    Add publish_date

--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
 
 
 class Post (models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=20000)
     draft = models.BooleanField(default=True)
+    publish_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
