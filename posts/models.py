a12484a086e10b71af5b43eadd5a0a76de4e93ea
Gouda01 <goda61@gmail.com>
2023-11-07 20:43:41 +0200

    Add category model with relationship

--- posts/models.py
+++ posts/models.py
@@ -9,17 +9,24 @@ from django.contrib.auth.models import User
 
 
 class Post (models.Model):
-    author = models.ForeignKey(User,related_name='post_author',on_delete=models.CASCADE)
+    author          = models.ForeignKey(User,related_name='post_author',on_delete=models.CASCADE)
+    title           = models.CharField(max_length=100)
+    content         = models.TextField(max_length=20000)
+    draft           = models.BooleanField(default=True)
+    publish_date    = models.DateTimeField(auto_now=True)
+    #publish_date   = models.DateTimeField(default=timezone.now)
+    tags            = TaggableManager()
+    image           = models.ImageField(upload_to='post')
+    category        = models.ForeignKey('Category',related_name='post_category',on_delete=models.SET_NULL,null=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+
+class Category (models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField(max_length=20000)
-    draft = models.BooleanField(default=True)
-    publish_date = models.DateTimeField(auto_now=True)
-    #publish_date = models.DateTimeField(default=timezone.now)
-    tags = TaggableManager()
-    image = models.ImageField(upload_to='post')
-    
-
-
 
     def __str__(self):
         return self.title
