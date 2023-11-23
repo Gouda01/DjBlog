f592f56f18801343ee4e8e20ca93421f03d38a98
Gouda01 <goda61@gmail.com>
2023-11-24 00:22:00 +0200

    Add comment form to post details

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render , redirect
-from .models import Post
-from .forms import PostForm
+from .models import Post , Comment
+from .forms import PostForm , CommentForm
 
 # Create your views here.
 
@@ -14,11 +14,22 @@ def post_list (request):
     }
     return render(request,'posts/post_list.html',context)
 
+
 def post_detail (request,pk) :
     data = Post.objects.get(id = pk)
-    
+    comments = Comment.objects.filter(post=data)
+    if request.method == 'POST' :
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            myform = form.save(commit=False)
+            myform.post = data
+            myform.save()
+    else :
+        form = CommentForm()
     context = {
-        'post' : data
+        'post' : data,
+        'comments': comments,
+        'form' : form,
     }
     return render (request , 'posts/post_detail.html' , context)
 
