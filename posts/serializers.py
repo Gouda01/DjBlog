88ba545b8aaad91b97846b55662e2c8b2d55b60a
Gouda01 <goda61@gmail.com>
2023-12-02 00:18:50 +0200

    show relationship in api

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,8 +1,11 @@
 
 from rest_framework import serializers
-from .models import Post
+from .models import Post 
 
 class PostSerializer (serializers.ModelSerializer):
+    author = serializers.StringRelatedField()
+    category = serializers.StringRelatedField()
+
     class Meta :
         model = Post
         fields = '__all__'
