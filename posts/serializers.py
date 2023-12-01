7ea2044f6fb9136f0d22bae5b0cb737c41d449e6
Gouda01 <goda61@gmail.com>
2023-12-02 00:24:05 +0200

    add relation as nasted object

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,9 +1,17 @@
 
 from rest_framework import serializers
 from .models import Post 
+from django.contrib.auth.models import User
+
+
+class AuthorSerializer (serializers.ModelSerializer) :
+    class Meta :
+        model = User
+        fields = ['id','username','email']
+
 
 class PostSerializer (serializers.ModelSerializer):
-    author = serializers.StringRelatedField()
+    author = AuthorSerializer()
     category = serializers.StringRelatedField()
 
     class Meta :
